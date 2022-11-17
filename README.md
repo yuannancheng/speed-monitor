a71410199091b1242c806f0a21586b36e9306993
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2022-11-17 13:25:53 +0600

    Repo created

---
+++ README.md
@@ -0,0 +1,51 @@
+# ⇅ Internet Speed Meter
+
+A simple and minimal Internet Speed Meter extension for the Gnome Shell.
+
+![Screenshot](screenshot.png)
+
+To use this extension, you will need
+
+- Gnome Shell 40 or later
+
+### Install from Gnome Extensions
+
+Visit [Internet Speed Meter - GNOME Shell Extensions](https://extensions.gnome.org/extension/2980/internet-speed-meter) 
+
+### Install from source
+
+1. Clone this repository
+
+   ```bash
+   git clone https://github.com/AlShakib/InternetSpeedMeter.git
+   ```
+
+2. Change current directory to repository
+
+   ```bash
+   cd InternetSpeedMeter
+   ```
+
+3. Now run
+
+   ```bash
+   chmod +x ./install.sh && ./install.sh
+   ```
+
+4. If you are using Wayland, log out and then log in. Otherwise press `ALT + F2` and then type `r` and press `Enter`.
+
+6. Enable **Internet Speed Meter** from the [Extension Manager](https://github.com/mjakeman/extension-manager)
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Thanks to
+
+- Project Icon is made by [Freepik](https://www.flaticon.com/authors/freepik) from [www.flaticon.com](https://www.flaticon.com)
+
+## License
+
+[GNU General Public License v3.0](LICENSE)
+
+Copyright © 2020 - 2022 [Al Shakib](https://alshakib.dev)
---
+++ src/extension.js
@@ -0,0 +1,107 @@
+/*
+ * Name: Speed-Buzz: Internet Speed Meter
+ * Description: A simple and minimal internet speed meter extension for Gnome Shell.
+ * Author: Al-Amin Islam Hridoy
+ * GitHub: https://github.com/HRIDOY-BUZZ/SpeedBuzz
+ * License: GPLv3.0
+ */
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Clutter = imports.gi.Clutter;
+const Mainloop = imports.mainloop;
+const Shell = imports.gi.Shell;
+
+const refreshTime = 1.0; // Set refresh time to one second.
+const unitBase = 1024.0; // 1 Gb == 1024Mb or 1Mb == 1024Kb etc.
+const units = ["Kbps", "Mbps", "Gbps", "Tbps"];
+const defaultNetSpeedText = 'BISM: ↓ -.-- --  ↑ -.-- --';
+const bit = 8; // 8 bits make a byte
+
+let prevUploadBits = 0,
+    prevDownloadBits = 0;
+let containerButton, netSpeedLabel, refreshLoop;
+
+function updateNetSpeed() {
+    if (netSpeedLabel) {
+        try {
+            let lines = Shell.get_file_contents_utf8_sync('/proc/net/dev').split('\n');
+            let uploadBits = 0;
+            let downloadBits = 0;
+            let line;
+            for (let i = 0; i < lines.length; ++i) {
+                line = lines[i].trim();
+                let column = line.split(/\W+/);
+
+                if (column.length <= 2)
+                    break;
+
+                if (column[0] != 'lo' && !isNaN(parseInt(column[1])) && !column[0].match(/^br[0-9]+/) && !column[0].match(/^tun[0-9]+/) && !column[0].match(/^tap[0-9]+/) && !column[0].match(/^vnet[0-9]+/) && !column[0].match(/^virbr[0-9]+/)) {
+                    uploadBits += (parseInt(column[9]) * bit);
+                    downloadBits += (parseInt(column[1]) * bit);
+                }
+            }
+
+            // Current upload speed
+            let uploadSpeed = (uploadBits - prevUploadBits) / (refreshTime * unitBase);
+
+            // Current download speed
+            let downloadSpeed = (downloadBits - prevDownloadBits) / (refreshTime * unitBase);
+
+            // Show upload + download = total speed on the shell
+            netSpeedLabel.set_text("SB:" + " ↓ " + getFormattedSpeed(downloadSpeed) + "  ↑ " + getFormattedSpeed(uploadSpeed));
+
+            prevUploadBits = uploadBits;
+            prevDownloadBits = downloadBits;
+            return true;
+        } catch (e) {
+            netSpeedLabel.set_text(defaultNetSpeedText);
+        }
+    }
+    return false;
+}
+
+function getFormattedSpeed(speed) {
+    let i = 0;
+    while (speed >= unitBase) { // Convert speed to Kb, Mb, Gb or Tb
+        speed /= unitBase;
+        i++;
+    }
+    return String(speed.toFixed(2) + " " + units[i]);
+}
+
+function enable() {
+    containerButton = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: false,
+        x_expand: true,
+        y_expand: false,
+        track_hover: false
+    });
+    netSpeedLabel = new St.Label({
+        text: defaultNetSpeedText,
+        style_class: 'netSpeedLabel',
+        y_align: Clutter.ActorAlign.CENTER
+    });
+    containerButton.set_child(netSpeedLabel);
+
+    Main.panel._rightBox.insert_child_at_index(containerButton, 0);
+    refreshLoop = Mainloop.timeout_add_seconds(refreshTime, updateNetSpeed);
+}
+
+function disable() {
+    if (refreshLoop) {
+        Mainloop.source_remove(refreshLoop);
+        refreshLoop = null
+    }
+    if (containerButton) {
+        Main.panel._rightBox.remove_child(containerButton);
+        containerButton.destroy();
+        containerButton = null;
+    }
+    if (netSpeedLabel) {
+        netSpeedLabel.destroy();
+        netSpeedLabel = null;
+    }
+}
